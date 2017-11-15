0e7173a0a588685500edbda1c5a559d510b5a041
Djavan Sergent <djavan.sergent@gmail.com>
2017-11-15 23:17:20 +0100

    update

--- homework/proofs/src/adt/proof_spec.lua
+++ homework/proofs/src/adt/proof_spec.lua
@@ -5,12 +5,16 @@ describe ("#theorem", function ()
     local Adt     = require "adt"
     local Natural = require "adt.natural"
     local Theorem = require "adt.theorem"
+    -- x + 0 = x
     local t1 = Theorem.axiom (Natural [Adt.axioms].addition_zero)
+    -- x + s(y) = s(x + y)
     local t2 = Theorem.axiom (Natural [Adt.axioms].addition_nonzero)
+    -- y + 0 = 0 + y
     local conjecture1 = Theorem.Conjecture {
       Natural.Addition { Natural._y, Natural.Zero {} },
       Natural.Addition { Natural.Zero {}, Natural._y }
     }
+    -- y + x = x + y
     local conjecture2 = Theorem.Conjecture {
       Natural.Addition { Natural._x, Natural._y },
       Natural.Addition { Natural._y, Natural._x }
@@ -28,13 +32,11 @@ describe ("#theorem", function ()
         local t3 = Theorem.substitutivity (Natural.Successor, {t})
         local t4 = Theorem.substitution   (t2, t2.variables [Natural._x])
         local t5 = Theorem.transitivity   (t3, Theorem.symmetry (t4))
-        local t6 = Theorem.substitutivity (Natural.Successor, {t1})
-        local t7 = Theorem.transitivity   (Theorem.symmetry (t5), t6)
-        local t8 = Theorem.substitution   (t1, t1.variables [Natural._x])
-        local t9 = Theorem.transitivity   (t7, Theorem.symmetry (t8))
-        return Theorem.symmetry (t9)
+        return Theorem.symmetry (t5)
       end
     })
+    print(conjecture2)
+    print(theoremy0)
   end)
 
 end)
